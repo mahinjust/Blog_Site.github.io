f02bd1c5380339bb5852b3d340aba47811215e35
Md. Ashav Noman Mahin <56822729+mahinjust@users.noreply.github.com>
2020-10-11 14:30:15 +0600

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,37 +1,20 @@
-"""first_project URL Configuration
+from django.urls import path
+from . import views
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
-from django.urls import include, path
-from django.contrib import admin
-from django.contrib.auth.views import LoginView,LogoutView
+urlpatterns=[
+                 path('',views.PostListView.as_view(),name='post_list'),
+                 path('about/',views.AboutView.as_view(),name='about'),
+                 path('post/(?p<pk>\d+)',views.PostDetailView.as_view(),name='post_detail'),
+                 path('post/new/',views.CreatePostView.as_view(),name='post_new'),
+                 path('post/(?p<pk>\d+)/edit/',views.PostUpdateView.as_view(),name='post_edit'),
+                 path('post/(?p<pk>\d+)/remove/',views.PostDeleteView.as_view(),name='post_remove'),
+                 path('drafts/',views.DraftListView.as_view(),name='post_draft_list'),
+                 path('post/(?p<pk>\d+)/comment/',views.add_comment_to_post,name='add_comment_to_post'),
+                 path('comment/(?p<pk>\d+)/approve/',views.comment_approve,name='comment_approve'),
+                 path('comment/(?p<pk>\d+)/remove/',views.comment_remove,name='comment_remove'),
+                 path('post/(?p<pk>\d+)/publish/',views.post_publish,name='post_publish')
 
 
-urlpatterns = [
-    path('admin/',admin.site.urls),
-    path('',include('blog.urls')),
-    path('accounts/login/', LoginView.as_view(), name='login'),
-    path('accounts/logout/', LogoutView.as_view(), name='logout',kwargs={'next_page':'/'})
+
 
             ]
-
-from django.conf import settings
-from django.urls import include, path
-
-if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns = [
-        path('__debug__/', include(debug_toolbar.urls)),
-    ] + urlpatterns
